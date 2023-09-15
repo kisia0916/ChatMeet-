5e31b6fd00024de6561091da0b93cf9adcdd484d
kisia0916 <fumiharuabe@gmail.com>
2023-09-15 18:47:49 +0900

    url change

--- public/doms/roomListDom.js
+++ public/doms/roomListDom.js
@@ -1,4 +1,4 @@
-const domain = "https://chatmeets.onrender.com/join/"
+const domain = "https://www.fumiapp.com/join/"
 function sanitizeInput(input) {
     // 文字列内の特殊文字をエスケープします
     return input.replace(/[&<>"'/]/g, (match) => {
