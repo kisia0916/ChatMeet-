eb8654d7dea8c1270df262ec383154a18d7596b6
kisia0916 <fumiharuabe@gmail.com>
2023-09-17 16:54:29 +0900

    error change

--- public/doms/roomListDom.js
+++ public/doms/roomListDom.js
@@ -16,39 +16,41 @@ function sanitizeInput(input) {
 const roomListDom = (roomList)=>{
     console.log(roomList)
     let html = roomList.map((i)=>{
-        if(i.roomName){
-            i.roomName = sanitizeInput(i.roomName)
-            let textColor = "white"
-            if(i.userList.length>=7){
-                textColor = "#FF5E5E"
-            }
-            return `
-            <div class="topSpace"></div>
-                <div class="roomCard">
-                    <a href="/join/${i.roomId}" class="link">
-                    </a>
-                    <div class="roomCardLeft">
-                        <img src="${i.icon}" class="roomIcon" />
-                    </div>
-                    <div class="roomCardRight">
-                        <div class="roomCardText">
-                            <span class="roomTitle">${i.roomName}</span>
+        try{
+            if(i.roomName){
+                i.roomName = sanitizeInput(i.roomName)
+                let textColor = "white"
+                if(i.userList.length>=7){
+                    textColor = "#FF5E5E"
+                }
+                return `
+                <div class="topSpace"></div>
+                    <div class="roomCard">
+                        <a href="/join/${i.roomId}" class="link">
+                        </a>
+                        <div class="roomCardLeft">
+                            <img src="${i.icon}" class="roomIcon" />
                         </div>
-                        <div class="roomCardInfos">
-                            <span class="material-symbols-outlined roomPersonIcon">
-                                person
-                            </span>
-                            <span class="roomCardNum" style="color:${textColor}">${i.userList.length}/${i.maxPlayer}</span>
-                            <textarea id="roomIdAria${i.roomId}" style="display:none">${domain+i.roomId}</textarea>
-                            <button id="${i.roomId}" class="RoomListlinkCopyButton" onclick="copyId(this.id,event)"><span class="material-symbols-outlined">
-                            link
-                        </span></button>
+                        <div class="roomCardRight">
+                            <div class="roomCardText">
+                                <span class="roomTitle">${i.roomName}</span>
+                            </div>
+                            <div class="roomCardInfos">
+                                <span class="material-symbols-outlined roomPersonIcon">
+                                    person
+                                </span>
+                                <span class="roomCardNum" style="color:${textColor}">${i.userList.length}/${i.maxPlayer}</span>
+                                <textarea id="roomIdAria${i.roomId}" style="display:none">${domain+i.roomId}</textarea>
+                                <button id="${i.roomId}" class="RoomListlinkCopyButton" onclick="copyId(this.id,event)"><span class="material-symbols-outlined">
+                                link
+                            </span></button>
+                            </div>
                         </div>
-                    </div>
-                </div>                        
+                    </div>                        
 
-            `
-        }
+                `
+            }
+        }catch{}
     }).join("")
     if(roomList.length == 0){
         html = `
