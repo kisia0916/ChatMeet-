a98044bab153d51887fb6c3dbd2d2e871f44ee79
fumi <93899734+kisia0916@users.noreply.github.com>
2023-10-27 06:12:36 +0000

    tokenのアップデート

--- public/api/apiRouter.js
+++ public/api/apiRouter.js
@@ -1,12 +1,15 @@
 const router = require("express").Router()
 const UserToken = require("./models/UserIpTokenModule")
 const {v4: uuidv4} = require("uuid")
+
 router.get("/givetoken",async(req,res)=>{
-    const userIP = req.headers['x-forwarded-for']//ip取得がうまくいってない
+    const userIPList = req.headers['x-forwarded-for'].split(",")
+    const userIP = userIPList[0]
+    console.log(userIP)
     try{
+        const token = uuidv4()
         const isMyIp = await UserToken.find({userIp:userIP})
         if(isMyIp.length==0){
-            const token = uuidv4()
             const newuser = await new UserToken({
                 userIp:userIP,
                 token:token
@@ -15,10 +18,16 @@ router.get("/givetoken",async(req,res)=>{
             console.log(newuser)
             return res.status(200).json({
                 yourIp:userIP,
-                token:token
+                token:token,
             })
         }else{
-            return res.status(400).json({mess:"finded your ip"})
+            await isMyIp.updateOne({
+                token:token
+            })
+            return res.status(200).json({
+                yourIp:userIP,
+                token:token
+            })
         }
     }catch(error){
         return res.status(500).json("error")
